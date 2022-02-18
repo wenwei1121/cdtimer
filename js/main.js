dc704b639c42ca8156d56be21e4df8519d6c3bf1
Alweis_Do <wenweiyang1121@gmail.com>
2022-02-18 12:29:23 +0800

    cdtimer-v2

--- js/main.js
+++ js/main.js
@@ -6,14 +6,16 @@ const seconds_display = document.getElementById("seconds")
 
 function countdown() {
     
-const summer_end_date = new Date("10 Oct 2021") // 設定未來時間
-const current_date = new Date() // 抓取現在時間
-const total_seconds = (summer_end_date - current_date) / 1000 // 以秒為單位 毫秒/1000=秒
-
+const summer_start_time = new Date("1 Jul 2022") // 設定未來時間
+// console.log(summer_start_time)
+const current_time = new Date() // 抓取現在時間
+// console.log(current_time)
+const total_seconds = (summer_start_time - current_time) / 1000 // 以秒為單位 毫秒/1000=秒
+console.log(total_seconds)
 const days = Math.floor(total_seconds / 3600 / 24)
 const hours = Math.floor(total_seconds / 3600) % 24
 const minutes = Math.floor(total_seconds / 60) % 60
-const seconds = Math.floor(total_seconds) %60
+const seconds = Math.floor(total_seconds) % 60
 
 days_display.innerHTML = days
 hours_display.innerHTML = hours
